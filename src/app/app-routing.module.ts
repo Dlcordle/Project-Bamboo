7d2edcd0b11b8cc4438c8c9016bcdf7289dab0ff
Caleb Kirschbaum <calebhk98@gmail.com>
2022-03-30 14:35:06 -0500

    idk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent } from './components/main/main.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
 
   // define the path that corresponds with the component that should be rendered
   {path: '', component:MainComponent, pathMatch: "full"}, // upon initializing the app, it loads the main component
-  {path: 'main', component: MainComponent},
+  {path: 'main', component: MainComponent },
+  {path: 'register', component: SignInComponent},
+  {path: 'login', component: SignUpComponent},
 
   // {path: 'register', component: RegisterComponent},
   // {path: 'all', component: AllComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
@@ -19,6 +20,7 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

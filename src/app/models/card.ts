aa067bef6af5e80cb00d24f01988b2dec3c943b9
Dlcordle <50706732+Dlcordle@users.noreply.github.com>
2022-03-31 11:26:31 -0400

    card object
    card.ts

---
+++ src/app/models/card.ts
@@ -0,0 +1,30 @@
+export class Card {
+    id : number;
+    subject: string;
+    question: string;
+    answer: string;
+    relevant_topics: [];
+    extra_resources: [];
+    relevant_cards: [];
+    created_by: string;
+
+    constructor(
+        id: number,
+        subject: string,
+        question: string,
+        answer: string,
+        relevant_topics: [],
+        extra_resources: [],
+        relevant_cards: [],
+        created_by: string
+    ) {
+        this.id = id;
+        this.subject = subject;
+        this.answer = answer;
+        this.question = question;
+        this.relevant_topics = relevant_topics;
+        this.extra_resources = extra_resources;
+        this.relevant_cards = relevant_cards;
+        this.created_by = created_by;
+    }
+}
